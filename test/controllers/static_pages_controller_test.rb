625464db7cd4c5d45301add7ae4d84a4f1c06c50
MaxMeira <maxdawis@gmail.com>
2015-05-15 01:58:08 -0300

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,31 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "Home | SOA - Sistema de Orientação Acadêmica"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "Ajuda | SOA - Sistema de Orientação Acadêmica"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "Sobre | SOA - Sistema de Orientação Acadêmica"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "Contato | SOA - Sistema de Orientação Acadêmica"
   end
 
   test "should get news" do
     get :news
     assert_response :success
+    assert_select "News | SOA - Sistema de Orientação Acadêmica"
   end
 
 end
